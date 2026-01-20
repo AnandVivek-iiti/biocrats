112118c43757ce8471b4fb2e18e266dadd78a314
Anand Vivek <anand1234.vivek@gmail.com>
2026-01-20 23:35:20 +0530

    Change server listen address to '0.0.0.0'

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -98,6 +98,6 @@ app.use((err, req, res, next) => {
 /* ======================
    START SERVER
    ====================== */
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0' , () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
